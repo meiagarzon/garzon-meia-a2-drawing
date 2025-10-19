1516eac7b97cb7d0b9e7415220b647b30836ecbf
meiagarzon <000970924@mohawkcollege.ca>
2025-10-19 00:10:28 -0400

    Add cloud and colour change function with keys

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
@@ -11,12 +11,14 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        Color yellow = new Color("#ebd9b0");
+        Color yellow    = new Color("#ebd9b0");
         Color lightBlue = new Color("#85b396");
-        Color aqua = new Color("#445e4e");
-        Color green = new Color("#abc123");
-        Color darkGreen = new Color("#1f2b24");
-        Color color = Random.Color();
+        Color aqua      = new Color("#445e4e");
+        Color green     = new Color("#abc123");
+        Color darkGreen = new Color("#1f2b24");      
+        Color color     = Random.Color();
+        Color[] berryColors = new Color[2];
+        Color[] cloudColors = new Color[2];
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -25,6 +27,10 @@ namespace MohawkGame2D
         {
             Window.SetTitle("2D Interactive Drawing");
             Window.SetSize(400, 400);
+            berryColors[0] = new Color("#0226ab");    //RED
+            berryColors[1] = new Color("#c20262");    //BLUE
+            cloudColors[0] = new Color("#ffffee");    //WHITE
+            cloudColors[1] = new Color("#666677");    //GREY
         }
 
         /// <summary>
@@ -33,21 +39,56 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(lightBlue);
-            DrawBushes(300, 33);
-
-            //change berry colour
-            if (Input.IsMouseButtonPressed(MouseInput.Left) ||
-                Input.IsKeyboardKeyPressed(KeyboardInput.Space))
-            {
-                color = Random.Color();
-            }
-
-            DrawBerry(180, 300);
-
             //draw sun
             Draw.FillColor = yellow;
             Draw.LineColor = Color.Clear;
             Draw.Circle(340, 60, 30);
+
+            //reference bushes and berries
+            DrawBushes(300, 0);
+            DrawBerry(180, 0, berryColors);
+            DrawCloud(80, 70, cloudColors);
+
+            //change berry colour
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.R))
+            {
+                color = berryColors[0];
+            }
+
+            else if (Input.IsKeyboardKeyPressed(KeyboardInput.B))
+            {
+                color = berryColors[1];
+            }
+
+            //change cloud colour
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.W))
+            {
+                color = cloudColors[0];
+            }
+
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.G))
+            {
+                color = cloudColors[1];
+            }
+        }
+
+        void DrawBerry(int x, int y, Color[] berryColors)
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                Draw.FillColor = color;
+                Draw.LineColor = Color.Clear;
+                Draw.Circle(20 * i, 305, 2);
+            }
+        }
+
+        void DrawCloud(int x, int y, Color[] cloudColors)
+        {
+            Draw.FillColor = color;
+            Draw.LineColor = Color.Clear;
+            Draw.Capsule(50, 80, 110, 80, 10);
+            Draw.Circle(85, 70, 20);
+            Draw.Circle(65, 75, 15);
         }
 
         void DrawBushes(int x, int y)
@@ -68,16 +109,6 @@ namespace MohawkGame2D
             Draw.FillColor = aqua;
             Draw.LineColor = Color.Clear;
             Draw.Rectangle(0, 310, 400, 200);
-        }
-
-        void DrawBerry(int x, int y)
-        {
-            for (int i = 0; i < 21; i++)
-            {
-                Draw.FillColor = color;
-                Draw.LineColor= Color.Clear;
-                Draw.Circle(20 * i, 305, 2);
-            }
         }
     }
 
